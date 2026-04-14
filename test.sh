5c71370c97b5c3f462f67ace944d54fe2a1b3cda
ayb <ayb@3hg.fr>
2026-04-14 15:07:17 +0100

    Changed test script.

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# usage : ./test.sh [typechecker]
+# usage: ./test.sh [typechecker] [test directory]
 
 err=0
 nb=0
 
-for file in ./pass/*.mle; do
+if [ "$#" -ne 2 ] || ! [ -x "$1" ] || ! [ -d "$2" ]; then
+    echo "usage: ./test.sh [typechecker] [test directory]"
+    exit 1
+fi
+
+for file in "$2/pass/"*.mle; do
     "$1" "$file" 2> "$file".err
     if [ "$?" = 1 ]; then
        err=$((err+1))
@@ -14,7 +19,7 @@ for file in ./pass/*.mle; do
     nb=$((nb+1))
 done
 
-for file in ./fail/*.mle; do
+for file in "$2/fail/"*.mle; do
     "$1" "$file" 2> "$file".err
     if [ "$?" = 0 ]; then
        err=$((err+1))
